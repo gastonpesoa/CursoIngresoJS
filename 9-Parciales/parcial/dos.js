e8ae8a23c63221965a16c6a4fc459bd2c0fe6a94
GastonPesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-06 20:54:41 -0300

    ejercicio 1 y 2
    calculo perimetro de un cuadrado y importe final mas iva

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,11 @@
 function Mostrar()
 {
-  
+	var importe;
+	var importeFinal;
+
+	var importe = prompt("Ingrese un valor","0");
+
+	importeFinal = importe * 1.21;
+
+	document.getElementById('importeFinal').value = importeFinal;
 }
--- 9-Parciales/parcial/uno.js
+++ 9-Parciales/parcial/uno.js
@@ -1,5 +1,12 @@
 
 function Mostrar()
 {
+	var base;
+	var resultado;
+
+	base = document.getElementById('laBase').value;
+	resultado = 4*base;
+
+	alert("El perimetro del cuadrado es: " + resultado);
 
 }
