aab4cbc33958f1ee64273eb417743488f34c602c
GastonPesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-06 00:57:18 -0300

    t

---
+++ jsFerreteFacturacion.js
@@ -0,0 +1,54 @@
+/*1.	Para el departamento de facturación:
+A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
+B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
+C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
+*/
+function Sumar () 
+{
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var suma;
+
+	precioUno = parseInt(document.getElementById('PrecioUno').value);
+	precioDos = parseInt(document.getElementById('PrecioDos').value);
+	precioTres = parseInt(document.getElementById('PrecioTres').value);
+
+	suma = precioUno + precioDos + precioTres;
+
+	alert(suma);
+
+}
+function Promedio () 
+{
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var promedio;
+
+	precioUno = parseInt(document.getElementById('PrecioUno').value);
+	precioDos = parseInt(document.getElementById('PrecioDos').value);
+	precioTres = parseInt(document.getElementById('PrecioTres').value);
+
+	promedio = (precioUno + precioDos + precioTres)/3;
+
+	alert(promedio);
+}
+function PrecioFinal () 
+{
+	var precioUno;
+	var precioDos;
+	var precioTres;
+	var suma;
+	var precioFinal;
+
+	precioUno = parseInt(document.getElementById('PrecioUno').value);
+	precioDos = parseInt(document.getElementById('PrecioDos').value);
+	precioTres = parseInt(document.getElementById('PrecioTres').value);
+
+	suma = precioUno + precioDos + precioTres;
+	precioFinal = suma + (suma * 0.21);
+
+	alert(precioFinal);
+	
+}
