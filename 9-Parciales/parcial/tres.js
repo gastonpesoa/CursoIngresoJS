eb8aa1ecd3609d069ecf21fae7ddd3fa4eeafde3
GastonPesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-07 14:57:00 -0300

    ejercicio 3
    perímetro rectangulo

--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,22 @@
 function Mostrar()
+
 {
+	var largo;
+	var ancho;
+	var perimetro;
+	var alambre;
+	
+	largo = document.getElementById('alrgo').value;
+	ancho = document.getElementById('ancho').value;
+
+	largo = parseInt(largo);
+	ancho = parseInt(ancho);
+	
+	perimetro = 2 * (largo + ancho);
+	alambre = 3 * perimetro;
+
+	alert("Se necesitan " + alambre + " metros de alambre para colocarle 3 hilos de alambrado a su perímetro. " );
+
+
 
 }
