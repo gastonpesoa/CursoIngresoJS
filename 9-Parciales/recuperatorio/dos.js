02e646b21450076e115aa0d3f80b7da6317e8c9d
GastonPesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-07 17:00:57 -0300

    ejercicio 2
    importe final - 25%

--- 9-Parciales/recuperatorio/dos.js
+++ 9-Parciales/recuperatorio/dos.js
@@ -1,4 +1,15 @@
 function Mostrar()
+
 {
-  
+ 
+	var importe;
+	var importeFinal;
+
+	importe = prompt("Ingrese el importe del producto", "0");
+	
+	importeFinal = importe * 0.75;
+
+	document.getElementById('importeFinal').value = importeFinal;
+
+
 }
