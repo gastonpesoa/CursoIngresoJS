f6e1f1dffb7b9eba5db09231b0af3b72b9369a8d
GastonPesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-07 13:00:43 -0300

    ejercicio 5
    ||, !()

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,22 @@
-function Mostrar()
+﻿function Mostrar()
+
 {
 //tomo la edad  
+	var edad;
+	
+	edad = document.getElementById('edad').value;
+/*
+	if(!(edad > 12 && edad < 18))
+	{
+	   alert("No es adolecente");
+	}
+
+
+*/
+	if(edad < 13 || edad > 17)
+	{
+	   alert("No es adolecente");
+	}
 
 
 }//FIN DE LA FUNCIÓN
