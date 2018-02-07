6dd67b467b50d63318c755332ccbb6f10a9aea33
GastonPesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-07 13:08:30 -0300

    ejercicio 6
    if(){}else{if(){}else{}}

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,6 +1,27 @@
-function Mostrar()
+﻿function Mostrar()
+
 {
 //tomo la edad  
+	var edad;
+	
+	edad = document.getElementById('edad').value;
+
+	if(edad < 13)
+	{
+	   alert("Es menor de edad");
+	}
+	else
+	{
+	   if(edad > 17)
+	   {
+	      alert("Es mayor de edad");
+	   }
+	   else
+	   {
+	      alert("Es adolecente");
+	   }
+	}
+
 
 
 
