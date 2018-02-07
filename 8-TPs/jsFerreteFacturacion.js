e9589f8e09bda85e6a62f69898ee74c93193a5dc
GastonPesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-07 11:58:58 -0300

    ejercicio 1
    corrección formula porcentaje

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -1,66 +1,139 @@
-/*1.	Para el departamento de facturación:
+﻿/*1.	Para el departamento de facturación:
+
+
 A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
+
+
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
+
+
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
+
+
 function Sumar () 
 {
+	
+
 	var precioUno;
+	
 	var precioDos;
+	
 	var precioTres;
+	
 	var resultado;
 
+	
+
 	precioUno = document.getElementById('PrecioUno').value;
+	
 	precioDos = document.getElementById('PrecioDos').value;
+	
 	precioTres = document.getElementById('PrecioTres').value;
 
+	
+
 	precioUno = parseInt(precioUno);
+	
 	precioDos = parseInt(precioDos);
+	
 	precioTres = parseInt(precioTres); 
+
+	
 
 	resultado = precioUno + precioDos + precioTres;
 
+	
+
 	alert(resultado);
+
+
 }
+
+
 
 function Promedio () 
 {
+	
+
 	var precioUno;
+	
 	var precioDos;
+	
 	var precioTres;
+	
 	var resultado;
 
+	
+
 	precioUno = document.getElementById('PrecioUno').value;
+	
 	precioDos = document.getElementById('PrecioDos').value;
+	
 	precioTres = document.getElementById('PrecioTres').value;
 
+
+	
 	precioUno = parseInt(precioUno);
+	
 	precioDos = parseInt(precioDos);
+	
 	precioTres = parseInt(precioTres);
 
+
+	
 	resultado = (precioUno + precioDos + precioTres)/3;
 
+	
+
 	alert(resultado);
+
+
 }
+
+
 function PrecioFinal () 
+
 {
+	
+
 	var precioUno;
+	
 	var precioDos;
+	
 	var precioTres;
+	
 	var resultado;
+	
 	var precioFinal;
 
+	
+
 	precioUno = document.getElementById('PrecioUno').value;
+	
 	precioDos = document.getElementById('PrecioDos').value;
+	
 	precioTres = document.getElementById('PrecioTres').value;
 
+	
+
 	precioUno = parseInt(precioUno);
+	
 	precioDos = parseInt(precioDos);
+	
 	precioTres = parseInt(precioTres);
 
+	
+
 	resultado = precioUno + precioDos + precioTres;
-	precioFinal = resultado + (resultado * 0.21);
+	
+	precioFinal = resultado * 1.21;
+
+
 
 	alert(precioFinal);
 	
+
+
 }
+
