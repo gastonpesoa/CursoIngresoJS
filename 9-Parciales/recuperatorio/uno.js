92d3a1ebbfa22f0e60b91a99668cb41ed153f4c0
GastonPesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-07 16:54:25 -0300

    ejercicio 1
    sup cuadrado

--- 9-Parciales/recuperatorio/uno.js
+++ 9-Parciales/recuperatorio/uno.js
@@ -1,5 +1,16 @@
 
 function Mostrar()
+
 {
+	var base;
+	var resultado;
+	
+	base = document.getElementById('laBase').value;
+
+	resultado = base*base;
+	
+	alert("La superficie del cuadrado es: " + resultado);
+
+
 
 }
