afe1c0840ac15540e805bb290cca1f181021a277
GastonPesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-07 12:46:23 -0300

    ejercicio 2
    if >

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,6 +1,18 @@
-function Mostrar()
+﻿function Mostrar()
+
 {
 //tomo la edad  
+	var edad;
+	
+	edad = document.getElementById('edad').value;
+
+	if(edad > 17)
+	{
+	   alert("Es mayor de edad");
+	}
+
+
+
 
 
 
