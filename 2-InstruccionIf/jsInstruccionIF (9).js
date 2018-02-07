09429643072b4771fb8f43f248eb47a1011228cd
GastonPesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-07 14:27:32 -0300

    ejercicio 9
    numero random entre 1 y 10

--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,14 @@
-function Mostrar()
+﻿function Mostrar()
+
 {
 	//Genero el número RANDOM entre 1 y 10 
+
 	
+	var resultado;
+	
+	resultado = Math.floor(Math.random() * 10) + 1;
+	alert(resultado);
+
+
 
 }//FIN DE LA FUNCIÓN
