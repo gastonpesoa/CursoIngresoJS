55a736c635d74abf6b37f6699d673a977e2c4a52
GastonPesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-07 15:11:08 -0300

    ejercicio 4
    prompt, if, document.write

--- 9-Parciales/parcial/cuatro.js
+++ 9-Parciales/parcial/cuatro.js
@@ -1,4 +1,34 @@
 function Mostrar()
+
 {
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno = prompt("Ingrese el primer numero: ", "");
+	numeroDos = prompt("Ingrese el segundo numero: ", "");
+
+	numeroUno = parseInt(numeroUno);
+	numeroDos = parseInt(numeroDos);
+
+	if(numeroUno == numeroDos)
+	{
+	   resultado = numeroUno * numeroDos;
+	}		
+	else
+	{
+	   if(numeroUno > numeroDos)
+	   {
+		resultado = numeroUno - numeroDos;
+	   }
+	   else
+	   {
+		resultado = numeroUno + numeroDos;
+	   }
+	}
+
+
+	document.write(resultado);
+
 
 }
