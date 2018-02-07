6c66d899bc299f534be0d4b22589e59df1bc14d8
GastonPesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-07 13:30:14 -0300

    ejercicio 8
    falta probarlo

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,32 @@
-function Mostrar()
+﻿function Mostrar()
+
 {
 //tomo la edad  
 
+
+	
+	var edad;
+	
+	var estadoCivil;
+
+
+	
+	edad = document.getElementById('edad').value;
+	
+	estadoCivil = document.getElementById("estadoCivil").value;
+
+
+	
+	if(edad > 17 && estadoCivil = "Soltero")
+	{	
+	   alert('Es soltero y no es menor.');
+	}
 	
 
 
+
+
+
 }//FIN DE LA FUNCIÓN
+
+//if (!(edad < 18 && estadoCivil != "Soltero")) 
