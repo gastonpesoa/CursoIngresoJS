980bcc63c81dc7fe7dd480c7e2c2c128a108cc65
GastonPesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-08 16:52:42 -0300

    ejercicio 2
    importeFinal + iva , prompt, getElement =

--- 9-Parciales/parcial2/2-entradaSalida.js
+++ 9-Parciales/parcial2/2-entradaSalida.js
@@ -1,7 +1,20 @@
-//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+﻿//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+
+
 function Mostrar()
+
 {
-	alert("Funciona 2-EntradaSalida");
 	
+
+	var importe;
+	var importeFinal;
+
+	importe = prompt("Ingrese el importe", "");
+
+	importeFinal = importe * 1.21;
+
+	document.getElementById('importe').value = importeFinal;
+
+
 }
 
