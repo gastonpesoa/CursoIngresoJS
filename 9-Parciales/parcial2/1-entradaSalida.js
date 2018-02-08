e8f9e35bb1ccc1501fe6639cb9cec470fe33219a
GastonPesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-08 16:48:09 -0300

    ejercicio 1
    perímetro triangulo equilátero, getElementeById, alert

--- 9-Parciales/parcial2/1-entradaSalida.js
+++ 9-Parciales/parcial2/1-entradaSalida.js
@@ -1,7 +1,20 @@
-//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+﻿//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+
+
 function Mostrar()
 {
-	alert("Funciona 1-EntradaSalida");
 	
+
+	var base;
+	var resultado;
+
+	base = document.getElementById('lado').value;
+	
+	resultado = 3 * base;
+
+	alert("El perimetro es: " + resultado);
+	
+
+
 }
 
