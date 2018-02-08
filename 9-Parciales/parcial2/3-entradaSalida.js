bd4447eeea6f6b86314a83658c7315023349d38c
GastonPesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-08 16:59:34 -0300

    ejercicio 3
    perímetro rectangulo, getElementById, parseInt, alert.

--- 9-Parciales/parcial2/3-entradaSalida.js
+++ 9-Parciales/parcial2/3-entradaSalida.js
@@ -1,7 +1,27 @@
-//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+﻿//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+
+
 function Mostrar()
+
 {
-	alert("Funciona 3-EntradaSalida");
 	
+	var ancho;
+	var largo;
+	var perimetro;
+	var resultado;
+
+	ancho = document.getElementById('ancho').value;
+	largo = document.getElementById('largo').value;
+
+	ancho = parseInt(ancho);
+	largo = parseInt(largo);
+
+	perimetro = 2 * (ancho + largo);
+	resultado = 3 * perimetro;
+
+	alert("Se necesitan " + resultado + " m de alambrado");
+	
+
+
 }
 
