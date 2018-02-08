899212cf929e91f8e6f5ef0954e86252f04f342f
GastonPesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-08 16:38:37 -0300

    ejercicio 4
    if, +, -, 0.

--- 9-Parciales/recuperatorio/cuatro.js
+++ 9-Parciales/recuperatorio/cuatro.js
@@ -1,4 +1,35 @@
 function Mostrar()
+
 {
+
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno = prompt("Ingrese el primer numero", "0");
+	numeroDos = prompt("Ingrese el segundo numero", "0");
+
+
+	numeroUno = parseInt(numeroUno);
+	numeroDos = parseInt(numeroDos);
+
+	resultado = numeroUno + numeroDos;
+
+	if(resultado < 0)
+	{
+	   document.write("negativo");
+	}
+	else
+	{
+	   if(resultado == 0)
+	   {
+		document.write("cero");
+	   }
+	   else
+	   {
+		document.write("positivo");
+	   }
+	}
+
 
 }
