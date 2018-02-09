2a027aacd1a7bba0d1ff0a736e9b117a43e36b08
GastonPesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-09 11:54:10 -0300

    ejercicio 4
    if *, +, -

--- 9-Parciales/parcial2/4-if.js
+++ 9-Parciales/parcial2/4-if.js
@@ -1,7 +1,38 @@
-//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+﻿//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+
+
 function Mostrar()
+
 {
-	alert("Funciona 4-if");
 	
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+	
+	numeroUno = prompt("Ingrese el primer numero", "0");
+	numeroDos = prompt("Ingrese el segundo numero", "0");
+
+	numeroUno = parseInt(numeroUno);
+	numeroDos = parseInt(numeroDos);
+
+	if(numeroUno == numeroDos)
+	{
+	   resultado = numeroUno * numeroDos;	
+	}
+	else
+	{
+	   if(numeroUno > numeroDos)
+	   {
+		resultado = numeroUno - numeroDos;
+	   }
+	   else
+	   {
+		resultado = numeroUno + numeroDos;
+	   }
+	}
+
+	document.write(resultado);
+
+
 }
 
