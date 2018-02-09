ff06a4c7c9b44461c62480461c66c56ccdc2a92f
GastonPesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-09 12:05:01 -0300

    ejercicio 2
    importe final + iva

--- 9-Parciales/recuperatorio2/2-entradaSalida.js
+++ 9-Parciales/recuperatorio2/2-entradaSalida.js
@@ -1,7 +1,19 @@
-//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+﻿//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+
 function Mostrar()
+
 {
-	alert("Funciona 2-EntradaSalida");
 	
+	var importe;
+	var importeFinal;
+
+	importe = prompt("Ingrese un importe", "0");
+	importeFinal = importe * 1.21;
+
+	document.getElementById('importe').value = importeFinal;
+
+	//alert("Funciona 2-EntradaSalida");
+	
+
 }
 
