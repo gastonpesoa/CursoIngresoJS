e122832f769944229a03e7a5d9490e7e8b27056d
Gaston Pesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-13 23:44:16 -0300

    ejercicio 5 y 6
    switch / for, condicion, array

--- 9-Parciales/parcial2/5-switch.js
+++ 9-Parciales/parcial2/5-switch.js
@@ -1,7 +1,75 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 5-Switch");
+	var mes;
+
+	mes = prompt("Ingrese un mes del año");
+
+	switch(mes)
+	{
+		case "enero":
+		{
+			alert("Veranito!!!!");
+			break;
+		}
+		case "febrero":
+		{
+			alert("Veranito!!!!");
+			break;	
+		}
+		case "marzo":
+		{
+			alert("extraño enero y febrero!!!");
+			break;	
+		}
+		case "abril":
+		{
+			alert("extraño enero y febrero!!!");
+			break;	
+		}
+		case "mayo":
+		{
+			alert("extraño enero y febrero!!!");
+			break;	
+		}
+		case "junio":
+		{
+			alert("extraño enero y febrero!!!");	
+			break;
+		}
+		case "julio":
+		{
+			alert("extraño enero y febrero!!!");	
+			break;
+		}		
+		case "agosto":
+		{
+			alert("extraño enero y febrero!!!");
+			break;	
+		}
+		case "septiembre":
+		{
+			alert("extraño enero y febrero!!!");	
+			break;
+		}
+		case "octubre":
+		{
+			alert("extraño enero y febrero!!!");	
+			break;
+		}
+		case "noviembre":
+		{
+			alert("extraño enero y febrero!!!");
+			break;	
+		}
+		case "diciembre":
+		{
+			alert("extraño enero y febrero!!!");	
+			break;
+		}
+	}
+
+	//alert("Funciona 5-Switch");
 	
 }
 
--- 9-Parciales/parcial2/6-iteraciones.js
+++ 9-Parciales/parcial2/6-iteraciones.js
@@ -1,7 +1,27 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 6-iteraciones");
+	var i;
+	var dia = [];
+
+	for(i = 0; i < 7; i++)
+	{
+		dia[i] = prompt("Ingrese el importe: ");
+
+		if(dia[i] == 0)
+		{
+			alert("El valor no debe ser igual a 0");
+			dia[i] = prompt("Vuelva a ingresarlo");
+		}
+		
+	}
+
+	dia.sort(function(a, b){return a - b});
+
+	alert("El mayor importe fue :" + dia[dia.length - 1] + 
+	" y el menor: " + dia[0]);
+
+	console.log(dia);
 	
 }
 
