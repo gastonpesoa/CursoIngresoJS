b34957c1548053c3d829395dd9ea50413cc4879b
Gaston Pesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-14 07:41:02 -0300

    ejercicio 5 y 6
    switch, for array min max

--- 9-Parciales/parcial/cinco.js
+++ 9-Parciales/parcial/cinco.js
@@ -1,4 +1,52 @@
 function Mostrar()
 {
+	var dia;
+
+	dia = prompt("Ingrese el dia de la semana");
+
+	switch(dia)
+	{
+		case "lunes":
+		{
+			alert("a trabajar !!!");
+			break;
+		}
+
+		case "martes":
+		{
+			alert("a trabajar !!!");
+			break;
+		}
+
+		case "miercoles":
+		{
+			alert("a trabajar !!!");
+			break;
+		}
+
+		case "jueves":
+		{
+			alert("a trabajar !!!");
+			break;
+		}
+
+		case "viernes":
+		{
+			alert("a trabajar !!!");
+			break;
+		}
+
+		case "sabado":
+		{
+			alert("es fin de semana");
+			break;
+		}
+
+		case "domingo":
+		{
+			alert("es fin de semana");
+			break;
+		}		
+	}
 
 }
--- 9-Parciales/parcial/seis.js
+++ 9-Parciales/parcial/seis.js
@@ -1,4 +1,27 @@
 function Mostrar()
 {
+	var i;
+	var array = [];
+
+	for (i = 0; i < 24; i++)
+	{
+		array[i] = prompt("Ingrese el importe de las ventas del dia " + i);
+	
+		if(array[i] == 0)
+		{
+			alert("El importe debe ser mayor a 0");
+			array[i] = prompt("Vuelva a ingresar el importe");
+		}
+	}
+
+	console.log(array);
+
+	array.sort(function(a, b){return a - b});
+
+	document.getElementById("importeFinal").value = "El importe mayor es: " + 
+	array[array.length - 1] + " y el menor es: " + array[0];
+
+	console.log(array);
+
 
 }
