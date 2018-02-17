ed5b886f5ed2a1680e9e7ddb1aa75e6ff2310ff5
Gaston Pesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-17 12:02:40 -0300

    ejercicio 4
    varias formas de hacerlo, todo con if, if + switc, switch + if, todo switch

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -1,30 +1,170 @@
 ﻿/*4.	Para el departamento de iluminación:
-Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
-
+Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 
+pesos final.
 
 A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
 
-B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
+B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un 
+descuento del 40 % y si es de otra marca el descuento es del 30%.
 
+C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” 
+se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
 
-C.	Si compra 4  lamparitas bajo consumo marca "ArgentinaLuz" o “FelipeLamparas” se hace un descuento del 25 % y si es de otra marca el descuento es del 20%.
+D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es 
+del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra 
+marca un 5%.
 
-
-D.	Si compra 3  lamparitas bajo consumo marca "ArgentinaLuz"  el descuento es del 15%, si es  “FelipeLamparas” se hace un descuento del 10 % y si es de otra marca un 5%.
-
-
-E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
+E.	Si el importe final con descuento suma más de $120  se debe sumar un 
+10% de ingresos brutos en informar del impuesto con el siguiente mensaje:
  ”Usted pago X de IIBB.”, siendo X el impuesto que se pagó. 
-
- */
-
-
+*/
 
 function CalcularPrecio () 
-
 {
+/*
+	//lo que cambia es el porcentaje
+
+	var cantidad;
+	var marca;
+	var precioFinal;
+	var precioUni;
+	var precioBruto;
+	var porcentajeDesc;
+	var iibb;
+
+	cantidad = document.getElementById('Cantidad').value;
+	marca = document.getElementById('Marca').value;
+	precioUni = 35;
+	precioBruto = cantidad * precioUni;
+	porcentajeDesc = 0;
+
+	if (cantidad > 5) 
+	{
+		porcentajeDesc = 0.5;
+	}
+	else
+	{
+		if (cantidad == 5) 
+		{
+			if (marca == "ArgentinaLuz") 
+			{
+				porcentajeDesc = 0.4;
+			}
+			else
+			{
+				porcentajeDesc = 0.3;
+			}
+		}
+		else
+		{
+			if (cantidad == 4) 
+			{
+				if (marca == "ArgentinaLuz" || marca == "FelipeLamparas") 
+				{
+					porcentajeDesc = 0.25;
+				}
+				else
+				{
+					porcentajeDesc = 0.2;
+				}
+				else
+				{
+					if (marca != "ArgentinaLuz" ) 
+					{
+
+					}
+				}
+			}
+		}
+	}//if (cantidad > 5) 
+
+	precioFinal = precioBruto - (precioBruto * porcentajeDesc);
+	
+	if (precioFinal > 120) 
+	{
+		iibb = precioFinal * 0.1;
+		precioFinal = precioFinal + iibb;		
+		alert("Usted pago " + iibb " de IIBB.")
+	}
+
+	document.getElementById("precioDescuento").value = precioFinal;
 
 
+*/
+	switch(cantidad)
+	{
+		case 1:
+		case 2:
+			porcentajeDesc = 0;
+			break;
+		case 3:
+			/*
+			switch(marca)
+			{
+				case "ArgentinaLuz"	:
+				porcetnae = x;
+				break;
+				case "Felipe":
+				porcetnae = x;
+				break;
+			}
+
+			*/
+		//copio pego if cantidad 3
+			break;
+		case 4:
+		/*
+		switch(marca)
+		{
+			case "Argentin":
+			case "Feilp":
+				porcenta
+				break;
+			default:
+				porcentajeDes 0.2;
+				break;
+		}
+
+
+		*/
+		//copio pego if cantidad 4
+		break;
+		case 5:
+
+			/*
+			switch(marca)
+			if (cantidad == 5) 
+			{
+			if (marca == "ArgentinaLuz") 
+			{
+				porcentajeDesc = 0.4;
+			}
+			else
+			{
+				porcentajeDesc = 0.3;
+			}
+		}
+		break;
+		case 6:
+
+	}
+
+/*
+	var precio;
+	var precioDescuento; 
+	
+	cantidad = parseInt(cantidad);
+	marca = document.getElementById('Marca').value;
+	precio = cantidad * 35;
+
+	switch(cantidad)
+	{
+		default:
+
+	}
+
+*/
+	/*
 	var cantidad;
 	var marca;
 	var precio;
@@ -35,7 +175,6 @@ function CalcularPrecio ()
 	marca = document.getElementById('Marca').value;
 	precio = cantidad * 35;
 	
-
 	cantidad = parseInt(cantidad);
 	
 	//A
@@ -44,7 +183,6 @@ function CalcularPrecio ()
 	{
 	   precioDescuento = precio;
 	}
-
 	else 
 	{
 	   precioDescuento = precio * 1.5;
@@ -97,6 +235,7 @@ function CalcularPrecio ()
 	}
 
 	document.getElementById('precioDescuento').value = precioDescuento;
+*/
 }
 
 
