09e40c7abdb79d2ac59c54059a4e45c9dff1a09e
Gaston Pesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-17 10:57:57 -0300

    ejercicio 2 y 3
    corregido

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -5,17 +5,12 @@ function Mostrar()
 
 	switch(mesDelAño)
 	{
-		
 		case "Febrero" :
-
 			alert("Este mes no tiene más de 29 días.");
 			break;
-
 		default :
-
 			alert("Este mes tiene 30 o más días");
 			break;
-		
 	}//switch(mesDelAño)
 		
 		
