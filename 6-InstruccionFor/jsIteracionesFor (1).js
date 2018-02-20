718e22e7ee7db59cd9e64ebf0ee8cee122e2f049
Gaston Pesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-20 20:35:53 -0300

    ejercicios comentados clase

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -2,12 +2,37 @@ function Mostrar()
 
 {
 	var i;
-
+	
 	for(i = 1; i < 11; i++)
 	{
+		document.write("break " + i + "<br>");
+	}
+}
+
+/*
+i es index, indice. viende de los arrays
+for(contador = 0; contador < 10; contador++)
+{
+	
+}
+
+var i = 0;
+
+	for(; ;)
+	{
+		i++;
 		document.write("El numero es " + i + "<br>");
+
+		if (i == 10) 
+		{
+			break;
+			document.write("break " + i + "<br>");
+		}	
+		if (i == 5) 
+		{
+			continue;
+		}
+		document.write("break" + i + "<br>");
 	}
 
-
-
-}
+*/
--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -1,7 +1,62 @@
 ﻿function Mostrar()
 
 {
-	var i;
+	var contadorDePositivos = 0;
+	var contadorDeNegativos = 0;
+	var contadorDeCeros = 0;
+	var contadorDePar = 0;
+	var contadorDeImpar = 0;
+	var contador50 = 0;
+
+	for (contador = -100; contador < 100; contador++)
+	{
+		if (contador > 0)
+		{
+			contadorDePositivos++;
+		}
+		else
+		{
+			if (contador < 0) 
+			{
+				contadorDeNegativos++;
+			}
+			else
+			{
+				contadorDeCeros++;
+			}
+		}
+
+		if (contador%2 == 0 && contador != 0) 
+		{
+			contadorDePar++
+		}
+		else
+		{
+			if (contador != 0) 
+			{
+				contadorDeImpar++;
+			}
+		}
+
+		switch (contador)
+		{
+			case 50:
+				contador50++;
+				break;
+		}
+	}
+
+	document.write("positivos " + contadorDePositivos + "<br>");
+	document.write("negativos " + contadorDeNegativos + "<br>");
+	document.write("ceros " + contadorDeCeros + "<br>");
+	document.write("pares " + contadorDePar + "<br>");
+	document.write("impares " + contadorDeImpar + "<br>");
+	document.write("50 " + contador50 + "<br>");
+
+}//FIN DE LA FUNCIÓN
+
+/*
+var i;
 	var numero;
 	var contador = 0;
 
@@ -21,10 +76,4 @@
 	}
 
 	document.write("Cantidad de numeros pares encontrados: " + contador);
-
-
-
-
-
-
-}//FIN DE LA FUNCIÓN
+*/
