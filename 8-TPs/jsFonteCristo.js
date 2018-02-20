649fc6d497b580b0ebcf406d72174d770ce80364
Gaston Pesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-20 19:40:30 -0300

    fonte cristo , falta el punto e

--- 8-TPs/jsFonteCristo.js
+++ 8-TPs/jsFonteCristo.js
@@ -25,26 +25,82 @@ function ComenzarIngreso ()
 function NumerosPares()
 {
 	var numero;
-	numero = document.getElementById('numero').value;
+	var contador = 0;
+	var i;
 
-	while()
+	numero = document.getElementById('numero').value;
+	numero = parseInt(numero);
+
+	for(i = numero; i > 0; i--)
 	{
-		
+		if (i%2 == 0) 
+		{
+			contador = contador + 1;
+		}
 	}
+	alert("La cantidad de numeros pares desde el numero ingresado hasta el cero es: " + contador);
 }
 
 function NumerosImpares()
 {
+	var numero;
+	var contador = 0;
+	var i;
 
+	numero = document.getElementById('numero').value;
+	numero = parseInt(numero);
+
+	for(i = numero; i > 0; i--)
+	{
+		if (i%2 !== 0) 
+		{
+			contador = contador + 1;
+		}
+	}
+	alert("La cantidad de numeros impares desde el numero ingresado hasta el cero es: " + contador);
 }
 
 function NumerosDivisibles()
 {
+	var numero;
+	var contador = 0;
+	var i;
 
+	numero = document.getElementById('numero').value;
+	numero = parseInt(numero);
+
+	for(i = 1; i < 101; i++)
+	{
+		if (i%numero == 0) 
+		{
+			contador = contador + 1;
+		}
+	}
+	alert("La cantidad de numeros divisibles, por el numero ingresado, desde el 1 hasta el 100 es: " + contador);
 }
 
 function VerificarPrimo()
 {
+	var i;
+	var numero;
+
+	numero = document.getElementById('numero').value;
+	numero = parseInt(numero);
+
+	for(i = 1; i < numero; i++)
+	{
+		if (numero / i == Math.round(numero / i) && i != 1 && i != numero) 
+		{
+			alert("Numero no primo");
+			break;
+		}
+			else
+		{
+			alert("Numero primo");
+			break;
+		}
+	
+	}
 
 }
 
