72378d36333cf81b3feded381d75500b885fef4d
Gaston Pesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-20 21:38:17 -0300

    bingo loco

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -2,6 +2,7 @@ function Mostrar()
 
 {
 	var numeroRandom;
+	var contador;
 	var contador0 = 0;
 	var contador1 = 0;
 	var contador2 = 0;
@@ -12,8 +13,19 @@ function Mostrar()
 	var contador7 = 0;
 	var contador8 = 0;
 	var contador9 = 0;
+	var porcen0;
+	var porcen1;
+	var porcen2;
+	var porcen3;
+	var porcen4;
+	var porcen5;
+	var porcen6;
+	var porcen7;
+	var porcen8;
+	var porcen9;
+	var iteraciones = 1000;
 
-	for(i = 0; i < 100; i++)
+	for(contador = 0; contador < iteraciones; contador++)
 	{
 		numeroRandom = Math.floor(Math.random() * 10);
 		//document.write(numeroRandom);
@@ -38,7 +50,17 @@ function Mostrar()
 				contador5++;
 				break;
 			case 6:
-				contador6++;
+				porcen6 = (contador6 / (iteraciones)) * 100;
+				console.log(porcen6);
+				if (porcen6 > 7) 
+				{
+					contador--;
+					continue;
+				}
+				else
+				{
+					contador6++;
+				}
 				break;
 			case 7:
 				contador7++;
@@ -52,16 +74,47 @@ function Mostrar()
 		}
 	}//for(i = 0; i < 100; i++)
 
-	document.write("0: " + contador0 + "<br>");
-	document.write("1: " + contador1 + "<br>");
-	document.write("2: " + contador2 + "<br>");
-	document.write("3: " + contador3 + "<br>");
-	document.write("4: " + contador4 + "<br>");
-	document.write("5: " + contador5 + "<br>");
-	document.write("6: " + contador6 + "<br>");
-	document.write("7: " + contador7 + "<br>");
-	document.write("8: " + contador8 + "<br>");
-	document.write("9: " + contador9 + "<br>");
+	porcen0 = (contador0/(contador + 1))*100;
+	porcen0 = Math.round(porcen0);
+	document.write("Cantidad de 0: " + contador0 + "---" + porcen0 + " %: " + "<br>");
+
+	porcen1 = (contador1/(contador + 1))*100;
+	porcen1 = Math.round(porcen1);
+	document.write("Cantidad de 1: " + contador1 + "---" + porcen1 + " %: " + "<br>");
+
+	porcen2 = (contador2/(contador + 1))*100;
+	porcen2 = Math.round(porcen2);
+	document.write("Cantidad de 2: " + contador2 + "---" + porcen2 + " %: " + "<br>");
+
+	porcen3 = (contador3/(contador + 1))*100;
+	porcen3 = Math.round(porcen3);
+	document.write("Cantidad de 3: " + contador3 + "---" + porcen3 + " %: " + "<br>");
+
+	porcen4 = (contador4/(contador + 1))*100;
+	porcen4 = Math.round(porcen4);
+	document.write("Cantidad de 4: " + contador4 + "---" + porcen4 + " %: " + "<br>");
+
+	porcen5 = (contador5/(contador + 1))*100;
+	porcen5 = Math.round(porcen5);
+	document.write("Cantidad de 5: " + contador5 + "---" + porcen5 + " %: " + "<br>");
+
+	porcen6 = (contador6/(contador + 1))*100;
+	porcen6 = Math.round(porcen6);
+	document.write("Cantidad de 6: " + contador6 + "---" + porcen6 + " %: " + "<br>");
+
+	porcen7 = (contador7/(contador + 1))*100;
+	porcen7 = Math.round(porcen7);
+	document.write("Cantidad de 7: " + contador7 + "---" + porcen7 + " %: " + "<br>");
+
+	porcen8 = (contador8/(contador + 1))*100;
+	porcen8 = Math.round(porcen8);
+	document.write("Cantidad de 8: " + contador8 + "---" + porcen8 + " %: " + "<br>");
+
+	porcen9 = (contador9/(contador + 1))*100;
+	porcen9 = Math.round(porcen9);
+	document.write("Cantidad de 9: " + contador9 + "---" + porcen9 + " %: " + "<br>");
+
+
 }
 
 		/*for(i = 1; i < 11; i++)
