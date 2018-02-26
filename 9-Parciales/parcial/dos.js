29a88186add3abc22d1a750c471d014e1f4775a3
Gaston Pesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-26 19:34:24 -0300

    ejercicio 1 y 2 clase 26/02
    ejercicios modelo parcial

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,13 +1,38 @@
+
+/*
+pedir precio y porcentaje de descuento q se va a ahcer
+resultado por id
+
+*/
 function Mostrar()
 {
 	var precioBruto;
+	var descuento;
+	var precioFinal;
+	var porcentaje;
+
+	precioBruto = prompt("Ingrese el precio: ","0");
+	precioBruto = parseInt(precioBruto);
+
+	porcentaje = prompt("Ingrese el porcentaje de descuento: ","0");
+
+	descuento = precioBruto * (porcentaje/100);
+
+	precioFinal = precioBruto - descuento;
+
+	document.getElementById("importeFinal").value = precioFinal;
+}
+
+
+
+/*var precioBruto;
 	var precioFinal;
 
 	precioBruto = prompt("Ingrese el importe","0");
 
 	precioFinal = precioBruto * 1.21;
 
-	document.getElementById("importeFinal").value = precioFinal;
+	document.getElementById("importeFinal").value = precioFinal;*/
 
 	/*var importe;
 	var importeFinal;
@@ -17,4 +42,3 @@ function Mostrar()
 	importeFinal = importe * 1.21;
 
 	document.getElementById('importeFinal').value = importeFinal;*/
-}
--- 9-Parciales/parcial/uno.js
+++ 9-Parciales/parcial/uno.js
@@ -1,12 +1,35 @@
 
+/*triangulo equilatero, perimetro y superficie*/
 function Mostrar()
 {
+	//defino variables
 	var base;
+	var altura;
+	var perimetro;
+	var superficie;
+
+	//tomo datos
+	altura = prompt("Ingrese la altura: ","0");
+	altura = parseInt(altura);
+
+	base = document.getElementById('laBase').value;
+	base = parseInt(base);
+
+	//realizo calculo
+	perimetro = 3 * base;
+	superficie = (base * altura) / 2;
+
+	//muestro datos
+	document.write("El perimetro del triangulo equilatero es: " + perimetro);
+	document.write("<br>" + "La superficie del triangulo equilatero es: " + superficie);
+}
+
+
+
+/*var base;
 	var resultado;
 
 	base = document.getElementById('laBase').value;
 	resultado = 4*base;
 
-	alert("El perimetro del cuadrado es: " + resultado);
-
-}
+	alert("El perimetro del cuadrado es: " + resultado);*/
