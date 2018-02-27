3d6b0eb338421191ce51b61cd4cf46648522c87a
Gaston Pesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-27 20:48:15 -0300

    ej clase 27/02
    peso temp animales

--- 9-Parciales/parcial2/8-iteraciones.js
+++ 9-Parciales/parcial2/8-iteraciones.js
@@ -1,10 +1,154 @@
-﻿//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
+/*
+pedir 
+1-nombre de un animal
+2-peso del animal - validar > 0
+3-temperatura del habitat en donde vive entre > -40 y < 40
 
+informar
+1-cantidad de temp pares ingresadas
+2-cantidad de temp impares
+3-nombre del animal mas pesado
+4-nombre del animal menos pesado
+5-cantidad de aniomales cuyo habitat tenga menos de 0 grados
+6-promedio de peso de todos los animales
+7- temp maxima y minima
+*/
 function Mostrar()
-
 {
+	//variables
+	var nombre;
+	var peso;
+	var temperatura;
 
-	//Defino variables.
+	var respuesta = "";
+
+	var masPesado;
+	var menosPesado;
+	var nombreMasPesado;
+	var nombreMenosPesado;
+
+	var temperaturaMaxima;
+	var temperaturaMinima;
+
+	var promedio;
+	var suma = 0;
+
+	//contadores
+	var contador = 0;
+	var cantidadTemperaturasPares = 0;
+	var cantidadTemperaturasImpares = 0;
+	var cantidadAnimalesHabitatsMenoresACero = 0;
+
+	//inicio y respuesta
+	while (respuesta != "no")
+	{
+		//ingreso nombre
+		nombre = prompt("Ingrese el nombre del animal","");
+		contador++;
+
+		//ingreso peso
+		peso = prompt("Ingrese el peso del animal: ","Mayor a cero.");
+		peso = parseInt(peso);
+
+		//valido peso
+		while (peso < 0 || isNaN(peso))
+		{
+			peso = prompt("Vuelva a ingresar el peso del animal: ","Mayor a cero.");
+			peso = parseInt(peso);
+		}
+
+		//ingreso temperatura
+		temperatura = prompt("Ingrese la temperatura del habitat del animal: ","Entre -40 y 40 grados.")
+		temperatura = parseInt(temperatura);
+
+		//valido temperatura
+		while (temperatura < -40 || temperatura > 40 || isNaN(temperatura))
+		{
+			temperatura = prompt("Vuelva a ingresar la temperatura del habitat del animal: ","Entre -40 y 40 grados.")
+			temperatura = parseInt(temperatura);
+		}
+
+		//cantidad de temperaturas pares
+		if (temperatura % 2 == 0 && temperatura != 0) 
+		{
+			cantidadTemperaturasPares++;
+		}
+
+		//cantidad de temperaturas impares
+		if (temperatura % 2 != 0 && temperatura != 0) 
+		{
+			cantidadTemperaturasImpares++;
+		}
+
+		//animal mas pesado y menos pesado
+		if (contador == 1) 
+		{
+			masPesado = peso;
+			nombreMasPesado = nombre;
+			menosPesado = peso;
+			nombreMenosPesado = nombre;
+		}
+		else
+		{
+			if (peso > masPesado) 
+			{
+				masPesado = peso;
+				nombreMasPesado = nombre;
+			}
+			if (peso < menosPesado) 
+			{
+				menosPesado = peso;
+				nombreMenosPesado = nombre;
+			}
+		}//if (contador == 1) 
+
+		//cantidad de temperaturas menor a cero
+		if (temperatura < 0) 
+		{
+			cantidadAnimalesHabitatsMenoresACero++;
+		}
+
+		//suma de pesos
+		suma = suma + peso;
+
+		//temperatura maxima y minima
+		if (contador == 1) 
+		{
+			temperaturaMaxima = temperatura;
+			temperaturaMinima = temperatura;
+		}
+		else 
+		{
+			if (temperatura > temperaturaMaxima) 
+			{
+				temperaturaMaxima = temperatura
+			}
+			if (temperatura < temperaturaMinima) 
+			{
+				temperaturaMinima = temperatura;
+			}
+		}//if (contador == 1) 
+
+		respuesta = prompt("¿Desea continuar?","Enter para si/'no' para parar");
+	}
+
+	//promedio
+	promedio = suma / contador;
+
+	//muestro datos
+	document.write("La cantidad de temperaturas pares ingresadas es: " + cantidadTemperaturasPares);
+	document.write("<br>" + "La cantidad de temperaturas impares ingresadas es: " + cantidadTemperaturasImpares);
+	document.write("<br>" + "El nombre del animal mas pesado es: " + nombreMasPesado);
+	document.write("<br>" + "El nombre del animal menos pesado es: " + nombreMenosPesado);
+	document.write("<br>" + "La cantidad de animales con habitats menores a cero grados es: " + cantidadAnimalesHabitatsMenoresACero);
+	document.write("<br>" + "El promedio de los pesos de los animales es: " + promedio);
+	document.write("<br>" + "La temperatura maxima es: " + temperaturaMaxima);
+	document.write("<br>" + "La temperatura minima es: " + temperaturaMinima);
+}	
+
+
+/*//Defino variables.
 	var numero;
 	var respuesta = "";
 	var promedio;
@@ -70,8 +214,4 @@ function Mostrar()
 	document.write("<br>" + "El número máximo es: " + numeroMaximo);
 	document.write("<br>" + "El número minimo es: " + numeroMinimo);
 
-//alert("Funciona 8-iteraciones");
-	
-
-}
-
+//alert("Funciona 8-iteraciones");*/
