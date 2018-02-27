bdab01a5807fe0ee405435bc9c242ba5bb8599f5
Gaston Pesoa <36174107+gastonpesoa@users.noreply.github.com>
2018-02-26 21:47:13 -0300

    ej 6, 7

--- 9-Parciales/recuperatorio/seis.js
+++ 9-Parciales/recuperatorio/seis.js
@@ -7,24 +7,37 @@ function Mostrar()
 
 	var contador = 0;
 	
-	while (contador < 50)
+	while (contador < 10)
 	{
 		peso = prompt("Ingrese el peso en kilos: ","0");
+		peso = parseInt(peso);
 
-		while (peso < 0)
+		while (peso <= 0)
 		{
 			peso = prompt("Ingrese un peso mayor a cero kilos: ","0");
+			peso = parseInt(peso);
 		}
+
 		contador++;
 
 		if (contador == 1)
 		{
-			
+			masPesado = peso;
+			menosPesado = peso;
 		}
-	}
-	
-
+		else
+		{
+			if (peso > masPesado) 
+			{
+				masPesado = peso; 
+			}
+			if (peso < menosPesado) 
+			{
+				menosPesado = peso;
+			}
+		}
+	}//while (contador < 10)
 
-
-
+	document.write("El contenedor mas pesado pesa " + masPesado + " kg.");
+	document.write("<br>" + "El contenedor menos pesado pesa " + menosPesado + " kg.");
 }
--- 9-Parciales/recuperatorio/siete.js
+++ 9-Parciales/recuperatorio/siete.js
@@ -1,4 +1,72 @@
+/*Realizar el algoritmo que permita el ingreso por prompt de 
+edades (validar entre 0 y 100) y 
+el sexo (validar el sexo “f” o “m”) de 100 personas, informar por alert: 
+a) el promedio de edad. 
+b) la edad más baja. 
+c) la cantidad de varones que su edad sea mayor o igual a 20.*/
+
 function Mostrar()
 {
+	//Defino variables
+	var edad;
+	var sexo;
+	var promedio;
+	var edadMenor;
 
+	//Defino contadores
+	var contador = 0;
+	var sumaEdades = 0;
+	var contadorVaronesEdadMayor = 0;
+
+	//Verifico cantidad
+	while (contador < 100)
+	{
+		//Pido la edad
+		edad = prompt("Ingrese la edad: ","0");
+		edad = parseInt(edad);
+
+		//Verifico la edad
+		while (edad < 0 || edad > 100 || isNaN(edad))
+		{
+			edad = prompt("Vuelva a ingresar la edad, entre 0 y 100: ","0");
+			edad = parseInt(edad);
+		}
+
+		//Sumo edades
+		contador++;
+		sumaEdades = sumaEdades + edad;
+
+		//Pido el sexo
+		sexo = prompt("Ingrese el sexo: ","f/m");
+
+		//Verifico el sexo
+		while (sexo !== "f" && sexo !== "m" || !isNaN(sexo))
+		{
+			sexo = prompt("Vuelva a ingresar el sexo, 'f' o 'm': ","f/m");
+		}
+
+		//Encuentro la edad menor
+		if (contador == 1) 
+		{
+			edadMenor = edad;
+		}
+		else 
+		{
+			if (edad < edadMenor) 
+			{
+				edadMenor = edad;
+			}
+		}//if (contador == 1) 
+
+		//Sumo varones mayores o igual a 20
+		if (sexo == "m" && edad >= 20) 
+		{
+			contadorVaronesEdadMayor++;
+		}
+	}//while (contador < 100)
+	
+	promedio = sumaEdades / contador;
+
+	//muestro datos
+	document.write()
 }
